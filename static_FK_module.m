3d766a7c957e64b33eb6f87ec71d35467091dee4
Mikhail V Volkov <74855766+mikhailVvolkov@users.noreply.github.com>
2024-05-22 14:24:54 +0200

    Add files via upload

--- static_FK_module.m
+++ static_FK_module.m
@@ -2,7 +2,10 @@ function [obs_ac, obs_dc] = static_FK_module(sc,lm,lp,sp)
     optical_cycle = [0:0.01:1]*2*pi/lp.w;
     F_ac=abs(lm.F.*sin(lp.w*optical_cycle'));
 
-    JDOS.dc = FK_JDOS(sc.XUV,sp.BG,sp.mu,0.5*lm.F); % For the DC we choose cycle-averaged field strength
+    %JDOS.dc = FK_JDOS(sc.XUV,sp.BG,sp.mu,0.5*lm.F); % For the DC we choose
+    %cycle-averaged field strength. Turned out to give a worse fit,
+    %probably due to nonlinearity. It is better to use peak field.
+    JDOS.dc = FK_JDOS(sc.XUV,sp.BG,sp.mu,lm.F); % For the DC we choose cycle-averaged field strength
     JDOS.ac = FK_JDOS(sc.XUV,sp.BG,sp.mu,F_ac);
     JDOS.zero = FK_JDOS(sc.XUV,sp.BG,sp.mu,0);
     
@@ -22,6 +25,8 @@ function [obs_ac, obs_dc] = static_FK_module(sc,lm,lp,sp)
     obs_dc.d_alpha = differential_profile(obs.dc.alpha, obs.eq.alpha);
     obs_dc.d_JDOS = differential_profile(JDOS.dc, JDOS.zero)/cnst.c_light^3;
     
-    %adiabatic_OD = differential_matrix(obs.ac.OD, obs.eq.OD);
-    %figure;pcolor(optical_cycle - max(optical_cycle)/2,sc.XUV,adiabatic_OD);shading interp;
+%     adiabatic_OD = differential_matrix(obs.ac.OD, obs.eq.OD,1);
+%     figure;pcolor(optical_cycle - max(optical_cycle)/2,sc.XUV,adiabatic_OD);shading interp;
+%     load('redblue.mat');colormap(redblue);colorbar;caxis([-0.2 0.2]);title('Adiabatic FKE');
+%     xlabel('time (fs)');ylabel('Photon energy (eV)');
 end
